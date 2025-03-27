b6c37b9dd3f86cb54b700a86e5d1d696fe2172af
Taoshif1 <taoshif2@gmail.com>
2025-03-27 17:43:43 +0600

    TS learning begins

---
+++ first-program/index.js
@@ -0,0 +1,9 @@
+var username = "Gazi Taoshif";
+var age = 20;
+console.log(username); // Output: Gazi Taoshif
+console.log(age); // Output: 25
+function add(number1, number2) {
+    return number1 + number2;
+}
+var num = add(15, 10);
+console.log(num); // Output: 25
