c6cd7b0340ecc4e11934d37724824f8f82ef18ed
Taoshif1 <taoshif2@gmail.com>
2025-03-27 20:10:24 +0600

    built in data type

---
+++ built-in-type/index.js
@@ -0,0 +1,10 @@
+// buiit in data type: number, string, boolean, object, array, tuple, 
+// enum, any, void, null, undefined, never
+var id; // any
+var userId = 101; // number
+var userName = "Gazi Taoshif"; // string
+var isUpdated = true; // boolean
+console.log(id); // Output: undefined
+console.log(userId); // Output: 101 
+console.log(userName); // Output: Gazi Taoshif
+console.log(isUpdated); // Output: true
