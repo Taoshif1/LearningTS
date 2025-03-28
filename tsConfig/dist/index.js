669f070eb73b0e176d896b6f17a59a33dce5df2a
Taoshif1 <taoshif2@gmail.com>
2025-03-28 17:17:49 +0600

    comments added

--- tsConfig/dist/index.js
+++ tsConfig/dist/index.js
@@ -3,6 +3,4 @@
 // tsConfig - mainly for tsconfig.json file
 
 
-// Ths dist file must be in the same directory as the src file
-// This is because the dist file is compiled from the src file
 console.log('Hello, from index ts!');
